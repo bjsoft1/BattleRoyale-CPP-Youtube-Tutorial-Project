031a1723b526af2c877e04b01656cf52500503eb
BijayAdhikari <BijayAdhikari@DESKTOP-T7P31EF>
2024-12-25 20:20:11 +0545

    Uploading Initial Projects

---
+++ Source/YoutubeBattleRoyal/YoutubeBattleRoyalGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "YoutubeBattleRoyalGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class YOUTUBEBATTLEROYAL_API AYoutubeBattleRoyalGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
